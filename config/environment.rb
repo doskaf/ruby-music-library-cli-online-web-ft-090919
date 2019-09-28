0d9ed75b9465b39e47b81b69aafd6b59e903b64f
Dominique Skaf <doskaf@yahoo.com>
2019-09-28 23:48:02 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,12 @@ module Concerns
       self.all.find {|item| item.name == name}
     end
     
-    def find_or_create_by_name
-      
+    def find_or_create_by_name(name)
+      if !(self.find_by_name(name))
+        self.create(name)
+      else
+        self.find_by_name(name)
+      end
     end
     
   end
