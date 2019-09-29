eec943fa20959776cdfb55696027135bbd8f93fa
Dominique Skaf <doskaf@yahoo.com>
2019-09-29 00:12:50 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,4 +40,11 @@ class MusicLibraryController
       end
   end
   
+  def list_songs
+    lib = self.library.sort_by {|song|song.name}
+    lib.each do |song|
+      puts "#{lib.index(song) + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
+  
 end
